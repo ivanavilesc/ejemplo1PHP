0e3d931b8e7b0bf341a4072dc8aef3d35a2f2abb
Ivan Aviles <>
2019-03-06 21:49:34 -0300

    primeros archivos

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Texto sin programación 
+        <br>
+        <?php
+            $saludo="Mundo Variable";       
+            echo "Hola Mundo !2";
+            echo "Hola ".$saludo;
+        ?>
+        <br>
+        Variables globales $_SERVER
+        <br>
+        <pre>
+        
+        <?php
+        var_dump($_SERVER)
+        /*
+        Texto comentado
+        */
+        ?>
+        </pre>
+        
+        
+    </body>
+</html>
